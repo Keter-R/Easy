812bfba47c8cda75762948263077c355ae63acee
karlis <2995621482@qq.com>
2023-05-12 18:52:22 +0800

    基本完成

--- ChatPage.cpp
+++ ChatPage.cpp
@@ -2,6 +2,21 @@
 #include "Protocol.h"
 #include <QMap>
 #include <QTimer>
+#include <QFile>
+#include <QFileDialog>
+#include <QDateTime>
+#include <QMenu>
+#include <QAction>
+#include <QMessageBox>
+#include <QTextDocument>
+#include <QTextCursor>
+#include <QTextCharFormat>
+#include <QTextImageFormat>
+#include <QTextBlock>
+
+
+// emoji: (\xf0\x9f\x98\x80) to (\xf0\x9f\x98\x8A)
+QString emojis[10] = { "\xf0\x9f\x98\x80", "\xf0\x9f\x98\x81", "\xf0\x9f\x98\x82", "\xf0\x9f\x98\x83", "\xf0\x9f\x98\x84", "\xf0\x9f\x98\x85", "\xf0\x9f\x98\x86", "\xf0\x9f\x98\x87", "\xf0\x9f\x98\x88", "\xf0\x9f\x98\x89" };
 
 ChatPage::ChatPage(QString user, QWidget* parent) :currentUserAccount(user), QWidget(parent)
 {
@@ -13,14 +28,57 @@ ChatPage::ChatPage(QString user, QWidget* parent) :currentUserAccount(user), QWi
 			return;
 		}
 		DataFormation::Message msg;
+		DataFormation::MessageContent content;
 		msg.sender = currentUserAccount;
 		msg.receiver = currentChaterAccount;
-		msg.content = ui.textContent->toPlainText();
+		content.type = "Text";
+		content.content = ui.textContent->toPlainText();
+		msg.content = DataFormation::toQString(content);
 		msg.time = QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss");
 		RawSolution::Protocol::Instance()->addMessage(msg);
 		ui.textContent->clear();
 		this->refresh();
 	});	
+	connect(ui.uploadButton, &QPushButton::clicked, this, [=]() {
+		QString fileName = QFileDialog::getOpenFileName(this, tr("Open File"), "", tr("Image Files (*.png *.jpg *.bmp)"));
+		if (fileName.isEmpty())
+		{
+			return;
+		}
+		QFile file(fileName);
+		if (!file.open(QIODevice::ReadOnly))
+		{
+			return;
+		}
+		// convert image to base64
+		QByteArray data = file.readAll().toBase64();
+		file.close();
+		DataFormation::MessageContent content;
+		content.type = "Image";
+		content.content = data;
+		DataFormation::Message msg;
+		msg.sender = currentUserAccount;
+		msg.receiver = currentChaterAccount;
+		msg.content = DataFormation::toQString(content);
+		msg.time = QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss");
+		RawSolution::Protocol::Instance()->addMessage(msg);
+		this->refresh();
+	});
+	connect(ui.emojiButton, &QPushButton::clicked, this, [=]() {
+		QMenu* menu = new QMenu(this);
+		QAction* action;
+		for (int i = 0; i < 10; i++)
+		{
+			action = new QAction(emojis[i], this);
+			connect(action, &QAction::triggered, this, [=]() {
+				QTextCursor cursor = ui.textContent->textCursor();
+				cursor.insertText(action->text());
+				ui.textContent->setTextCursor(cursor);
+			});
+			menu->addAction(action);
+		}
+		menu->exec(QCursor::pos());
+	});
 }
 
 ChatPage::~ChatPage()
--- MessageListWidgetItem.cpp
+++ MessageListWidgetItem.cpp
@@ -1,4 +1,5 @@
 #include "MessageListWidgetItem.h"
+#include <QPushButton>
 
 MessageListWidgetItem::MessageListWidgetItem(DataFormation::User sender, DataFormation::Message message, bool isSelf, QWidget* parent)
 	: QWidget(parent)
@@ -6,14 +7,53 @@ MessageListWidgetItem::MessageListWidgetItem(DataFormation::User sender, DataFor
 	ui.setupUi(this);
 	ui.avatarLabel->setPixmap(QPixmap::fromImage(sender.getRoundAvatar(40)));
 	ui.timeLabel->setText(message.time);
+	DataFormation::MessageContent content = DataFormation::toMessageContent(message.content);
+	if (content.type == "Text")
+	{
+		QLabel* textLabel = new QLabel(content.content, this);
+		textLabel->setWordWrap(true);
+		textLabel->setAlignment(Qt::AlignLeft);
+		ui.contentWidget->layout()->addWidget(textLabel);
+	}
+	else if (content.type == "Image")
+	{
+		QWidget* imageWidget = new QWidget(this);
+		QVBoxLayout* layout = new QVBoxLayout(imageWidget);
+		QPushButton* imageBtton = new QPushButton(imageWidget);
+		imageBtton->setFlat(true);
+		imageBtton->setStyleSheet("border: none;");
+		imageBtton->setIconSize(QSize(250, 250));
+		imageBtton->setIcon(QPixmap::fromImage(DataFormation::toQImage(content.content)).scaled(250, 250, Qt::KeepAspectRatio));
+		layout->addWidget(imageBtton);
+		imageWidget->setLayout(layout);
+		ui.contentWidget->layout()->addWidget(imageWidget);
+		connect(imageBtton, &QPushButton::clicked, [=]()
+			{
+				QWidget* imageWidget = new QWidget();
+				QVBoxLayout* layout = new QVBoxLayout(imageWidget);
+				QPushButton* imageBtton = new QPushButton(imageWidget);
+				imageBtton->setFlat(true);
+				imageBtton->setStyleSheet("border: none;");
+				imageBtton->setIcon(QPixmap::fromImage(DataFormation::toQImage(content.content)));
+				imageWidget->setBaseSize(QPixmap::fromImage(DataFormation::toQImage(content.content)).scaled(1000, 1000, Qt::KeepAspectRatio).size());
+				imageBtton->setIconSize(QSize(imageWidget->width(), imageWidget->height()));
+				layout->addWidget(imageBtton);
+				imageWidget->setLayout(layout);
+				imageWidget->show();
+			});
+
+
+	}
 	if (isSelf)
 	{
 		this->setLayoutDirection(Qt::RightToLeft);
+		ui.contentWidget->setLayoutDirection(Qt::RightToLeft);
 		ui.timeLabel->setAlignment(Qt::AlignRight);
 	}
 	else
 	{
 		this->setLayoutDirection(Qt::LeftToRight);
+		ui.contentWidget->setLayoutDirection(Qt::LeftToRight);
 		ui.timeLabel->setAlignment(Qt::AlignLeft);
 	}
 }
